5409b984834b2728014c0b8d9946a6adf9ecb9b4
furaizi <furachannel16@gmail.com>
2025-02-15 17:30:06 +0200

    Add mapping relationship between Car and Seat

--- src/train-service/src/main/java/com/example/trainservice/model/Car.java
+++ src/train-service/src/main/java/com/example/trainservice/model/Car.java
@@ -1,5 +1,6 @@
 package com.example.trainservice.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,15 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue
     private Long id;
     private Long trainId;
     private int number;
     private CarType type;
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
     private List<Seat> seats;
     private int totalSeats;
 }
--- src/train-service/src/main/java/com/example/trainservice/model/Seat.java
+++ src/train-service/src/main/java/com/example/trainservice/model/Seat.java
@@ -1,5 +1,6 @@
 package com.example.trainservice.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Seat {
+    @Id
+    @GeneratedValue
     private Long id;
-    private Long carId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id")
+    private Car car;
+
     private int number;
     private SeatType type;
 }
